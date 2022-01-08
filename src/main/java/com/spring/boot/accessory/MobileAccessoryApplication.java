f894ad421c37bc1edf8042d6af1315e98ed90e20
shalabhraj1990 <shalabhrajetc@gmail.com>
2022-01-08 12:34:30 +0530

    porject code added

---
+++ src/main/java/com/spring/boot/accessory/MobileAccessoryApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.accessory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileAccessoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileAccessoryApplication.class, args);
+	}
+
+}
